4765697b8c727250b3d9bece291bc1813fe2d4ad
Daniel Cornelio <jdcornelio89@gmail.com>
2022-11-14 23:33:27 -0600

    comprobar pwd

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,23 +1,51 @@
 import Usuario from "../models/Usuario.js";
+import generarId from "../helpers/generarId.js";
 
-const registrar = async(req, res)=>{
-    //evitar registros duplicados
-    const {email}= req.body;
-    const existeusuario = await Usuario.findOne({email});
-    if(existeusuario){
-        const error = new Error('Usuario ya registrado')
-        return res.status(400).json({msg:error.message});
-    }
-    try {
-        const usuario = new Usuario(req.body);
-        const usuarioAlmacenado = await usuario.save()
-        res.json(usuarioAlmacenado);
-    } catch (error) {
-        console.log(error);
-    }
+const registrar = async (req, res) => {
+  //evitar registros duplicados
+  const { email } = req.body;
+  const existeusuario = await Usuario.findOne({ email });
+  if (existeusuario) {
+    const error = new Error("Usuario ya registrado");
+    return res.status(400).json({ msg: error.message });
+  }
+  try {
+    const usuario = new Usuario(req.body);
+    usuario.token = generarId();
+    const usuarioAlmacenado = await usuario.save();
+    res.json(usuarioAlmacenado);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
+const autenticar = async (req, res) => {
+  const { email, password } = req.body;
+  //comprobar si usuario exite
+  const usuario = await Usuario.findOne({ email });
+  if (!usuario) {
+    const error = new Error("El usuario no existe");
+    return res.status(404).json({ msg: error.message });
+  }
 
-export{
-    registrar
+  console.log(usuario);
+
+  //comprobar si el usuario esta confirmado
+  if (!usuario.confirmado) {
+    const error = new Error("Tu cuenta no ha sido confirmada");
+    return res.status(403).json({ msg: error.message });
+  }
+  //comprobar usuario
+  if (await usuario.comprobarPassword(password)) {
+    res.json({
+        _id:usuario._id,
+        nombre:usuario.nombre,
+        email:usuario.email
+    })
+  } else {
+    const error = new Error("El password es incorrecto");
+    return res.status(403).json({ msg: error.message });
+  }
 };
+
+export { registrar, autenticar };
